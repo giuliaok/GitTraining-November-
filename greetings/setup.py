f12ef858cbfe5ff35e74433f0aaa80a08d4aa5c1
GiuliaO <gocchini@turing.ac.uk>
2019-11-06 14:54:38 +0000

    added setup again

--- greetings/setup.py
+++ greetings/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "Greetings",
+    name = "dungeons",
     version = "0.1.0",
     packages = find_packages(exclude=['*test']),
 )
-Overwriting greetings/setup.py
+Overwriting dungeons/setup.py
